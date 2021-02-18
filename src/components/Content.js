d5bb2a42289fce22652171b9e5f6e742e2fa81f9
ihjacobs925 <ihjacobs925@gmail.com>
2021-02-18 16:20:17 -0600

    Header

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,14 @@
+import { Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Auth from './pages/Auth';
+import Profile from './pages/Profile';
+
 const Content = (props) => {
     return (
         <main>
-            <h1>HERE BE CONTENT</h1>
+            <Route exact path='/' component={Home} />
+            <Route path='/auth' component={Auth} />
+            <Route path='/profile' component={Profile} />
         </main>
     );
 }
--- src/components/partials/Header.js
+++ src/components/partials/Header.js
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
 const Header = (props) => {
+    let style = {
+        backgroundColor: 'chartreuse',
+        borderBottom: '3px solid black',
+        margin: 0,
+        padding: '1em 0'
+    }
     return (
-        <header>
+        <header style={style}>
             <h2>HEADER</h2>
+            <nav>
+                {/* TODO: Conditional Link Rendering */}
+                <Link to='/'>Home</Link>{' | '}
+                <Link to='/auth'>Login or Signup</Link>{' | '}
+                <Link to='/profile'>Account</Link>
+            </nav>
         </header>
     );
 }
